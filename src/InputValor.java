6cdcc1a29c57855c1a13c67d8b609b6bfc4d1593
Bruna Soncini <brusoncini@gmail.com>
2023-07-09 12:59:05 -0300

    add opção continuar ou sair

--- src/InputValor.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class InputValor {
-    Double inputValor = Double.parseDouble(JOptionPane.showInputDialog("Insira um valor"));
-     Double valorInserido = inputValor;
-
-
-}
